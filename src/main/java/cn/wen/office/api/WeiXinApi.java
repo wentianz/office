6dd7d31f1fc173ebd8da1f90c5abcb92f7229f6f
wentianz <654491056@qq.com>
2019-01-27 16:51:23 +0800

    a

--- src/main/java/cn/wen/office/api/WeiXinApi.java
+++ src/main/java/cn/wen/office/api/WeiXinApi.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface WeiXinApi{
-    @GET("cgi-bin/user/info")
+    @GET("https://api.weixin.qq.com/cgi-bin/user/info")
     Call<JSONObject> getUserInfo(@Query("access_token") String access_token, @Query("openid") String openid, @Query("lang") String lang);
 
 }
